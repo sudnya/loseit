45f0413979343711d028d367a69b859cdc0df6ab
sudnya <mailsudnya@gmail.com>
2017-06-08 17:32:53 -0700

    ENH: stay logged in

--- food-journal/v1/config/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-    'facebookAuth' : {
-        'clientID'      : '318410955256174', // your App ID
-        'clientSecret'  : '8eb19c399bfba9e39d9f8d864a7bc1c5', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
-    }
-
-};
