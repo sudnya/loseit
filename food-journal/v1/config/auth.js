28101547d8f85b4acc547a526797eadc81fba3c9
sudnya <mailsudnya@gmail.com>
2017-07-12 17:44:39 -0700

    ENH: new app link in auth

--- food-journal/v1/config/auth.js
+++ food-journal/v1/config/auth.js
@@ -5,6 +5,6 @@ module.exports = {
  'facebookAuth' : {
      'clientID'        : '318410955256174', // your App ID
      'clientSecret'    : '8eb19c399bfba9e39d9f8d864a7bc1c5', // your App Secret
-     'callbackURL'     : 'https://cryptic-peak-93625.herokuapp.com/auth/facebook/callback'
+     'callbackURL'     : 'https://food-journal-fb.herokuapp.com/auth/facebook/callback'
  }
 };
