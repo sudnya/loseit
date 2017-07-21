4aee1f79e80375104448f2e84533f1db5513ef66
sudnya <mailsudnya@gmail.com>
2017-07-21 14:36:00 -0700

    BUG: remove fb scope email

--- food-journal/v1/routes/routes.js
+++ food-journal/v1/routes/routes.js
@@ -75,7 +75,7 @@ var _ = require('underscore');
     // FACEBOOK ROUTES =====================
     // =====================================
     // route for facebook authentication and login
-    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+    app.get('/auth/facebook', passport.authenticate('facebook'));
 
     // handle the callback after facebook has authenticated the user
     app.get('/auth/facebook/callback',
