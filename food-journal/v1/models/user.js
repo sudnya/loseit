415cda62a4c1cbbe73be462d3d5565281bcbe425
sudnya <mailsudnya@gmail.com>
2017-07-21 14:28:52 -0700

    RM: login page, change user schema

--- food-journal/v1/models/user.js
+++ food-journal/v1/models/user.js
@@ -7,13 +7,20 @@ var userSchema = mongoose.Schema({
 
     facebook         : {
         id           : String,
-        token        : String,
-        email        : String,
-        name         : String
+        token        : String
     }
 
 });
 
+// generating a hash		
+userSchema.methods.generateHash = function(password) {		
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);		
+};		
+		
+// checking if password is valid		
+userSchema.methods.validPassword = function(password) {		
+    return bcrypt.compareSync(password, this.local.password);		
+};
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
