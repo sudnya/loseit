fdc27adffbfe521b509eae34da10a38bd9ac9a16
sudnya <mailsudnya@gmail.com>
2017-07-20 17:19:19 -0700

    BUG: not just user.id

--- food-journal/v1/config/passport.js
+++ food-journal/v1/config/passport.js
@@ -17,13 +17,13 @@ module.exports = function(passport) {
     // used to serialize the user for the session
     
     passport.serializeUser(function(user, done) {
-        done(null, user.id);
+        done(null, user.facebook.id);
     });
 
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
         User.findById(id, function(err, user) {
-            done(err, user.id);
+            done(err, user.facebook.id);
         });
     });
 
